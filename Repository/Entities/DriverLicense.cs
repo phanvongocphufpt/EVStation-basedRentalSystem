8ae52e569cbce334c4d7ebaeb0fa1ab85d7ee80d
Dương Thành Thanh Duy <duongduy12314@gmail.com>
2025-10-16 14:35:24 +0700

    Update DriverLiense.cs

--- Repository/Entities/DriverLicense.cs
+++ Repository/Entities/DriverLicense.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Repository.Entities.Enum;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,18 +8,17 @@ using System.Threading.Tasks;
 
 namespace Repository.Entities
 {
-    public class CitizenId
+    public class DriverLicense
     {
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
-        public string CitizenIdNumber { get; set; }
-        public DateOnly BirthDate { get; set; }
         public string ImageUrl { get; set; }
-        public string Status { get; set; }
+        public DocumentStatus Status { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
+
     }
 }
