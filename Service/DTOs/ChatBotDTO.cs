ac0a1982ed5ded979eabc6aa4e0dff54d343c9aa
Dương Thành Thanh Duy <duongduy12314@gmail.com>
2025-11-11 16:14:41 +0700

    update chatbot

---
+++ Service/DTOs/ChatBotDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.DTOs
+{
+        public class ChatRequest
+        {
+            public string Message { get; set; }
+            public bool ShortAnswer { get; set; } = false; // trả lời ngắn hay dài
+        }
+
+        public class ChatResponse
+        {
+            public string Reply { get; set; }
+        }
+    }
+
