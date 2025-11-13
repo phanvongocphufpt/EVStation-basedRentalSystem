26242babfc1c531d83e314cae874dd9698ec4d92
Dương Thành Thanh Duy <duongduy12314@gmail.com>
2025-11-13 13:31:51 +0700

    test

---
+++ Repository/Entities/ChatMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Entities
+{
+    public class ChatMessage
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int SenderId { get; set; }          // Id của người gửi
+        public int ReceiverId { get; set; }        // Id của người nhận
+        public string Message { get; set; }
+        public DateTime SentAt { get; set; } = DateTime.Now;
+
+        public bool IsRead { get; set; } = false;
+    }
+}
