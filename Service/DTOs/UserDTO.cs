8e618fce21309c297cb1a1f5ce20dba3d6c282be
Dương Thành Thanh Duy <duongduy12314@gmail.com>
2025-11-25 21:34:50 +0700

    Revert "uddate user"
    This reverts commit 99455d1d6de482ebfe5d3a81a7da5328f0b5880e.

--- Service/DTOs/UserDTO.cs
+++ Service/DTOs/UserDTO.cs
@@ -34,7 +34,6 @@ namespace Service.DTOs
         public string Email { get; set; }
         public string Password { get; set; }
         public string FullName { get; set; }
-        public string PhoneNumber { get; set; }
     }
     public class UpdateCustomerNameDTO
     {
