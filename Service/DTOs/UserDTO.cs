99455d1d6de482ebfe5d3a81a7da5328f0b5880e
Dương Thành Thanh Duy <duongduy12314@gmail.com>
2025-11-25 21:15:03 +0700

    uddate user

--- Service/DTOs/UserDTO.cs
+++ Service/DTOs/UserDTO.cs
@@ -34,6 +34,7 @@ namespace Service.DTOs
         public string Email { get; set; }
         public string Password { get; set; }
         public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
     }
     public class UpdateCustomerNameDTO
     {
