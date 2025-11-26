f9e07726727f8194a239055e903877dc6f196d06
Tiger_Kon <hotrachkhoa123@gmail.com>
2025-11-27 02:14:30 +0700

    p

--- EVStation-basedRentalSystem/Controllers/RentalOrderController.cs
+++ EVStation-basedRentalSystem/Controllers/RentalOrderController.cs
@@ -81,5 +81,127 @@ namespace EVStation_basedRentalSystem.Controllers
             }
             return BadRequest(result);
         }
+<<<<<<< Updated upstream
+=======
+        [HttpPut("ConfirmTotal")]
+        [Authorize(Roles = "Admin,Staff")]
+        public async Task<IActionResult> ConfirmTotal(int orderId)
+        {
+            var result = await _rentalOrderService.ConfirmTotalAsync(orderId);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPut("ConfirmOrderPayment")]
+        [Authorize(Roles = "Admin,Staff")]
+        public async Task<IActionResult> ConfirmOrderPayment([FromBody] ConfirmOrderPaymentDTO dto)
+        {
+            var result = await _rentalOrderService.ConfirmOrderPaymentAsync(dto);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpDelete("CancelOrder")]
+        [Authorize(Roles = "Admin,Staff,Customer")]
+        public async Task<IActionResult> CancelOrder([FromForm] int orderId)
+        {
+            var result = await _rentalOrderService.CancelOrderAsync(orderId);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpDelete("CancelOrderForStaff")]
+        [Authorize(Roles = "Admin,Staff")]
+        public async Task<IActionResult> CancelOrderForStaff([FromForm] int orderId)
+        {
+            var result = await _rentalOrderService.CancelOrderForStaffAsync(orderId);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        //[HttpGet("Checkout/PaymentCallbackVnpay")]
+        //[HttpPost("Checkout/PaymentCallbackVnpay")]
+        //public async Task<IActionResult> PaymentCallbackVnpay()
+        //{
+        //    var result = await _rentalOrderService.ProcessVnpayCallbackAsync(Request.Query);
+
+        //    if (result.IsSuccess)
+        //        return Redirect($"http://localhost:7200/payment-success?orderId={result.OrderId}");
+
+        //    return Redirect($"http://localhost:7200/payment-failed?code={result.VnPayResponseCode ?? "99"}");
+        //}
+        //[HttpPost("api/payment/vnpay-ipn")]
+        //public async Task<IActionResult> VnpayIpn()
+        //{
+        //    var result = await _rentalOrderService.ProcessVnpayIpnAsync(Request.Query);
+
+        //    return Ok(new
+        //    {
+        //        RspCode = result.IsSuccess ? "00" : "99",
+        //        Message = result.Message
+        //    });
+        //}
+        [HttpPost("api/payment/confirm-orderdeposit-manual")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmPaymentManual([FromBody] ConfirmPaymentDto dto)
+        {
+            // FE sẽ gửi lên TxnRef và ResponseCode
+            var result = await _rentalOrderService.ProcessVnpayCallbackManualAsync(dto.TxnRef, dto.ResponseCode);
+
+            if (result.IsSuccess)
+            {
+                return Ok(new { success = true, message = "Thanh toán thành công!", orderId = result.OrderId });
+            }
+
+            return Ok(new { success = false, message = result.Message });
+        }
+
+        public class ConfirmPaymentDto
+        {
+            public string TxnRef { get; set; } = string.Empty;
+            public string ResponseCode { get; set; } = string.Empty;
+        }
+        [HttpPost("AddContactToOrder")]
+        [Authorize(Roles = "Admin,Staff,Customer")]
+        public async Task<IActionResult> AddContactToOrder([FromBody] AddContactToOrderDTO dto)
+        {
+            var result = await _rentalOrderService.AddContactToOrderAsync(dto);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("GetContactFromOrder")]
+        [Authorize(Roles = "Admin,Staff,Customer")]
+        public async Task<IActionResult> GetContactFromOrder(int orderId)
+        {
+            var result = await _rentalOrderService.GetContactFromOrderDTO(orderId);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPut("UpdateContact")]
+        [Authorize(Roles = "Admin,Staff,Customer")]
+        public async Task<IActionResult> UpdateContact([FromBody] GetContactFromOrderDTO dto)
+        {
+            var result = await _rentalOrderService.UpdateContact(dto);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+>>>>>>> Stashed changes
     }
 }
